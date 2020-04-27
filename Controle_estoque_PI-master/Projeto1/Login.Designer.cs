0da4ed29b0ca40ae459705ff0a7a6e60dab24af4
felipe fernandes <felipefernandes1407@outlook.com>
2020-04-27 20:27:34 -0300

    alterações de visual para controle de estoque

---
+++ Controle_estoque_PI-master/Projeto1/Login.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Projeto1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(148, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(148, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Senha";
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(228, 61);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(119, 20);
+            this.txtusuario.TabIndex = 1;
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(228, 114);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(118, 20);
+            this.txtsenha.TabIndex = 1;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnlogin.BackColor = System.Drawing.Color.White;
+            this.btnlogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnlogin.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnlogin.FlatAppearance.BorderSize = 0;
+            this.btnlogin.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btnlogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnlogin.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogin.ForeColor = System.Drawing.Color.Black;
+            this.btnlogin.Location = new System.Drawing.Point(152, 186);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 25);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "Logar";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnsair.BackColor = System.Drawing.Color.White;
+            this.btnsair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnsair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnsair.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnsair.FlatAppearance.BorderSize = 0;
+            this.btnsair.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btnsair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnsair.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnsair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsair.ForeColor = System.Drawing.Color.Black;
+            this.btnsair.Location = new System.Drawing.Point(272, 186);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 25);
+            this.btnsair.TabIndex = 2;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = false;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Imprint MT Shadow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Info";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(495, 280);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Controle_estoque_PI-master/Projeto1/Login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Projeto1
+{
+    public partial class Login : Form
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //chamar banco de dados 
+                MySqlConnection con = new MySqlConnection("server=localhost;port=3307;User Id=root; database=login;passaword=usbw");
+                con.Open();  //abrir banco de dados 
+                MessageBox.Show("Conectado");
+                con.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show("deu ruim");
+            }
+
+            this.Hide();
+            Form f = new mPrincipal();
+            f.Closed += (s, args) => this.Close();
+            f.Show();
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
